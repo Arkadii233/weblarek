9019e186acdd73f729738faeff9583ae7121879e
Arkadii Galchenko <galchenkoav7@gmail.com>
2026-05-18 12:19:15 +0800

    final2

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,50 +1,45 @@
-export type TPayment = "card" | "cash" | null;
-
-export type TCheckError = {
-  payment?: string;
-  email?: string;
-  phone?: string;
-  address?: string;
-};
-
 export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
-  get<T extends object>(uri: string): Promise<T>;
-  post<T extends object>(
-    uri: string,
-    data: object,
-    method?: ApiPostMethods,
-  ): Promise<T>;
+    get<T extends object>(uri: string): Promise<T>;
+    post<T extends object>(
+        uri: string,
+        data: object,
+        method?: ApiPostMethods,
+    ): Promise<T>;
 }
 
 export interface IProduct {
-  id: string;
-  description: string;
-  image: string;
-  title: string;
-  category: string;
-  price: number | null;
+    id: string;
+    title: string;
+    image: string;
+    category: string;
+    price: number | null;
+    description: string;
 }
+
+export type TPayment = "card" | "cash";
 
 export interface IBuyer {
-  payment: TPayment | null;
-  email: string;
-  phone: string;
-  address: string;
-}
-
-export interface IProductList {
-  items: IProduct[];
-  total: number;
+    payment: TPayment | null;
+    email: string;
+    phone: string;
+    address: string;
 }
 
 export interface IOrder extends IBuyer {
-  items: string[];
-  total: number;
+    total: number;
+    items: string[];
 }
 
 export interface IOrderResult {
-  id: string;
-  total: number;
+    id: string;
+    total: number;
 }
+
+export interface IProductsResponse {
+    total: number;
+    items: IProduct[];
+}
+
+export type TBuyerErrors = Partial<Record<keyof IBuyer, string>>;
